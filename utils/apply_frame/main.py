5670e0ba433ffe12726f6db86c6cd7d027c698c4
progressify <a.porcelli90@gmail.com>
2024-03-08 12:01:14 +0100

    done apply frame script

--- utils/apply_frame/main.py
+++ utils/apply_frame/main.py
@@ -1,9 +1,10 @@
 import click
 import os
 from PIL import Image, ImageOps
+from tqdm import tqdm
 
 
-def apply_frame(image_path, frame_path):
+def apply_frame(frame_path, image_path):
     # applico la cornice
     frame = Image.open(frame_path)
     image = Image.open(image_path)
@@ -19,19 +20,35 @@ def compress_image(image_path, compression):
     image.save(image_path, quality=compression)
 
 
+def is_image_3000x3000(image_path):
+    image = Image.open(image_path)
+    width, height = image.size
+    return width == 3000 and height == 3000
+
+
 @click.command()
-@click.argument("images", nargs=-1, type=str)
+@click.argument("frame", type=str)
+@click.argument("folder", type=str)
 @click.option("--compression", default=75, type=int, help="Livello di compressione (da 1 a 100)")
-def main(images, compression):
-    frame_path = os.path.join("./media", "cornice.png")
+def main(frame, folder, compression):
+    if not os.path.exists(frame):
+        raise Exception(f"La cornice '{frame}' non esiste")
 
-    for image in images:
-        image_path = os.path.join("./media", f"PIC_{image}.png")  # TODO
+    if not is_image_3000x3000(frame):
+        raise Exception("La cornice deve avere dimensioni 3000x3000")
 
-        apply_frame(image_path, frame_path)
-        compress_image(image, compression)
+    if not os.path.exists(folder):
+        raise Exception(f"La cartella '{folder}' non esiste")
 
-        print(f"Immagine '{image}' compressa con successo!")
+    images = os.listdir(folder)
+    for image in tqdm(images):
+        image_path = os.path.join(folder, image)
+        if not is_image_3000x3000(image_path):
+            print(f"L'immagine {image} non è di dimensioni 3000x3000\nskipping...")
+            continue
+
+        apply_frame(frame, image_path)
+        compress_image(image_path, compression)
 
 
 if __name__ == "__main__":
